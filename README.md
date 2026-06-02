64d59e068b372af69a68354193e9a97252ed6706
MarkLevkovich <levmarkpost@gmail.com>
2026-06-02 16:42:57 +0300

    init!

---
+++ README.md
@@ -0,0 +1,11 @@
+                _  _____ ________
+               | |/ /| |/ __|/ __|
+               | ' <\| |\__ \ (__
+               |_|\_\|_|___/\___|
+
+                      KISS
+            keep it simple, stupid.
+
+kiss is a terminal text editor. not an IDE, not emacs, not vim.
+just an editor. it does syntax highlighting, file trees, undo/redo,
+and gets out of your way.
---
+++ src/app.py
@@ -0,0 +1,58 @@
+from argparse import ArgumentParser
+from pathlib import Path
+from textual.app import App
+from textual.containers import Horizontal
+from textual.highlight import guess_language
+from textual.widgets import TextArea, DirectoryTree
+
+class Kiss(App):
+
+    BINDINGS = [
+        ("ctrl+s", "save_file"),
+        ("ctrl+q", "quit")
+    ]
+
+    def __init__(self, folder):
+        super().__init__()
+        self.folder = folder
+        self.file = None
+
+    def compose(self):
+        yield Horizontal(
+            TextArea("", id="editor"),
+            DirectoryTree(self.folder)
+        )
+
+    def _on_directory_tree_file_selected(self, event):
+        path: Path = event.path
+        if not path.is_file():
+            return
+        self.file = path
+
+        text_editor = self.query_one("#editor")
+        text_editor.text = self.file.read_text()
+
+        language_name = guess_language(text_editor.text, self.file)
+        text_editor.language = language_name
+
+    def action_save_file(self):
+        if self.file is None:
+            return
+        editor = self.query_one("#editor")
+        self.file.write_text(editor.text)
+
+if __name__=="__main__":
+    parser = ArgumentParser("KISS")
+    parser.add_argument("folder", type=Path)
+
+    args = parser.parse_args()
+    folder: Path = args.folder
+
+    if not folder.exists():
+        raise FileNotFoundError(f"Bad path: {folder}")
+
+    if not folder.is_dir():
+        folder = folder.parent
+
+    app = Kiss(folder=folder)
+    app.run()
