52a3f761ed82e5dc8d1257370f59a1c6e0bda6d4
MarkLevkovich <levmarkpost@gmail.com>
2026-09-24 20:35:31 +0300

    better cp example

--- examples/kiss_templates/cp-example.py
+++ examples/kiss_templates/cp-example.py
@@ -1,13 +1,38 @@
 import sys
 
-
-def rdl():
-    return sys.stdin.readline().strip()
+sys.setrecursionlimit(200000)
 
 
-def rd():
-    return sys.stdin.read().strip()
+def _input_stream():
+    for line in sys.stdin:
+        for token in line.split():
+            yield token
 
 
-def pr(s: str):
-    sys.stdout.write(s)
+_stream = _input_stream()
+
+
+def rs():
+    return next(_stream)
+
+
+def ri():
+    return int(next(_stream))
+
+
+def rli(n=None):
+    if n is None:
+        return list(map(int, sys.stdin.readline().split()))
+    return [int(next(_stream)) for _ in range(n)]
+
+
+def pr(*args, sep=" ", end="\n"):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def solve():
+    pass
+
+
+if __name__ == "__main__":
+    solve()
